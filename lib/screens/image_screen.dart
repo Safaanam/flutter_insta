9471421a3c9516c2c13294e0f44157d3ca82a8c0
Safa Anam <safa.anam@digital.datamatics.com>
2021-03-02 17:01:44 +0530

    image screen modified

--- lib/screens/image_screen.dart
+++ lib/screens/image_screen.dart
@@ -34,37 +34,6 @@ class _ImageScreenState extends State<ImageScreen> {
     });
   }
 
-  void _showPicker(context) {
-    showModalBottomSheet(
-        context: context,
-        builder: (BuildContext bc) {
-          return SafeArea(
-            child: Container(
-              child: new Wrap(
-                children: <Widget>[
-                  new ListTile(
-                      leading: new Icon(Icons.photo_library),
-                      title: new Text('Photo Library'),
-                      onTap: () {
-                        _imgFromGallery();
-                        Navigator.of(context).pop();
-                      }),
-                  new ListTile(
-                    leading: new Icon(Icons.photo_camera),
-                    title: new Text('Camera'),
-                    onTap: () {
-                      _imgFromCamera();
-                      Navigator.of(context).pop();
-                    },
-                  ),
-                ],
-              ),
-            ),
-          );
-        }
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
